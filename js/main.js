9b19b99cfb805ffa1a1ab7c8bdd6d9589f1a21be
Expiers1 <dobudoglo.maxim@gmail.com>
2020-10-19 18:00:21 +0300

    Con one

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+
+$('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"> <img class = "ri" src="img/right.png" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img class = "le" src="img/left.png" alt=""></button>',
+});
+
+});
+$(function(){
+
+    $('.slider__inners').slick({
+        nextArrow: '<button type="button" class="slick-btns slick-nexts"> <img src="img/right.png" alt="">    </button>',
+        prevArrow: '<button type="button" class="slick-btns slick-prevs"><img src="img/left.png" alt=""></button>'
+    })
+    
+    });
+    $(function(){
+
+        $('.slider__innerss').slick({
+            nextArrow: '<button type="button" class="slick-btns slick-nextss"> <img src="img/right.png" alt="">   </button>',
+            prevArrow: '<button type="button" class="slick-btns slick-prevss"><img src="img/left.png" alt=""></button>',
+        });
+        
+        });
